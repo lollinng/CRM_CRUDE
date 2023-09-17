7c47e0d75f7825629394679aa918cddd2e00bb06
lollinng <pratameshjadhav@gmail.com>
2023-09-17 16:13:15 +0530

    added Registration functionalities

--- crud_crm/website/views.py
+++ crud_crm/website/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+
+from .forms import SignUpForm
+
 # Create your views here.
 
 def home(request):
@@ -27,3 +30,22 @@ def logout_user(request):
     logout(request)
     messages.success(request,"You Have Been Logged Out...")
     return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
